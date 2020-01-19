8e2c19a77bb768192b2d95795e7440b4831a0e40
horix7 <horix77777@gmail.com>
2020-01-19 02:37:18 +0200

    removed un necessary files

--- UI/anouncememt.js
+++
@@ -1,8 +0,0 @@
-const anouncementName = document.querySelector("#anouncement_inputs_name")
-const anouncementStartDate = document.querySelector("#anouncement_inputs_Sdate")
-const anouncementEndtDate = document.querySelector("#anouncement_inputs_Edate")
-const anouncementDescription = document.querySelector("#anouncement_inputs_Description")
-// const anouncementStatus = document.querySelector()
-
-
-let anouncement = [anouncementName, anouncementDescription, anouncementStartDate, anouncemen]
