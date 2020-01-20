651ce4425441cd3e9a8bf864ac39fd052a7dd492
horix7 <horix77777@gmail.com>
2020-01-20 08:46:51 +0200

    improved the UI

--- UI/js/anouncememt.js
+++ UI/js/anouncememt.js
@@ -12,8 +12,13 @@ for (let x = 0; x< runIt; x++) {
     ${status}
     </div>
     </div>
-        <div class = "inline_div"> ${outputInfo[1]}
-        </div>  
+        <div class = "inline_div"><div> ${outputInfo[1]} </div>
+            <div>
+            <span class="start"><i class="fa fa-dot-circle-o" aria-hidden="true" id = "dot"></i> Starting ${outputInfo[2]}</span>
+            <span class="end"><i class="fa fa-dot-circle-o" aria-hidden="true" id = "dot"></i> Ending ${outputInfo[3]}</span> 
+            </div>
+        </div> 
+        
     <div>
         <i class="fa fa-ellipsis-v" id="readMore"></i>
         <!-- fa fa-ellipsis-v -->
